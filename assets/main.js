81f0706966b8beb92908d57a97bf932cef6122f6
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-08 16:09:17 +0200

    add bonus with variable switch

--- assets/main.js
+++ assets/main.js
@@ -4,8 +4,31 @@ const buttonEl = document.getElementById('turn_on')
 const imgEl = document.getElementById('white_lamp')
 console.log(buttonEl);
 console.log(imgEl);
+/* let switchIt = true
+
+buttonEl.addEventListener('click', function(){
+    
+    if(switchIt){
+    imgEl.src = './assets/img/yellow_lamp.png';
+    buttonEl.innerHTML = 'Spegni'
+    }
+    else{
+        imgEl.src = './assets/img/white_lamp.png';
+    buttonEl.innerHTML = 'Accendi'
+    }
+    switchIt = !switchIt
+}) */
 
 
 buttonEl.addEventListener('click', function(){
+    let imgEl = 'switch'.includes()
+    if('switch'.includes('lamp')){
     imgEl.src = './assets/img/yellow_lamp.png';
+    buttonEl.innerHTML = 'Spegni'
+    }
+    
+    else{
+        imgEl.src = './assets/img/white_lamp.png';
+    buttonEl.innerHTML = 'Accendi'
+    }
 })
